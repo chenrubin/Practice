dabbf681089ec7032484efe9c31656f4f031cc7a
chenrubin <52496330+chenrubin@users.noreply.github.com>
2020-08-05 10:07:21 +0300

    Update bohanLargestNumber.cpp

--- CPP/bohanLargestNumber.cpp
+++ CPP/bohanLargestNumber.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <cstring>
+#include <stddef.h>
+#include <algorithm>
+#include <vector>
+#include <string>     // std::string, std::to_string
 
+//stoi(str1)
+bool compareFunc(int i, int j)
+{
+    return std::to_string(i) + std::to_string(j) > 
+           std::to_string(j) + std::to_string(i); 
+}
+
+std::string LargestNumber(int *arr, size_t size)
+{
+    std::vector<int> vec(arr, arr + size);
+ 
+    std::sort(vec.begin(), vec.end(), compareFunc);
+    
+    std::cout << "\n";
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        std::cout << vec[i];
+    }
+
+    std::string str;
+
+    return str;
+}
+
+int main()
+{
+    int arr[] = {3,30,34,5,9};
+    LargestNumber(arr, 5);
+
+
+
+    return 0;
+}
